45a12c42088693381ce4c54f0fd1e1f060959c86
Jesse Garcia <jeg6187@gmail.com>
2018-08-25 12:54:44 -0600

    still not finshed with http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularGolfScoreCard';
+  coursesListArray = [];
+  courseListUrl = 'https://uxcobra.com/golfapi/courses.txt';
+
+  constructor(private http: HttpClient) {
+    this.setCourseList();
+  }
+  getConfig(url) {
+    return this.http.get(url);
+  }
+  setCourseList() {
+   const stuff = this.http.get(this.courseListUrl)
+    .subscribe((data) => {
+      const courses = data.courses;
+      courses.forEach(course => {
+        this.coursesListArray.push(course);
+      });
+    });
+  }
 }
